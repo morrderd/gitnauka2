4316a40adc2d980d7a22d306d8909ea3d8fa0ff8
Mateusz Ćwiląg <mateuszcwilag@gmail.com>
2020-03-28 19:07:03 +0100

    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitnauka2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
